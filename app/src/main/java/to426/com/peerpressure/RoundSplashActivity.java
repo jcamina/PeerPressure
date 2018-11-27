211d33663750e1d9bad838136b55d7eba2362679
Jordan Camina <jcamina@umich.edu>
2018-11-26 23:49:18 -0500

    Round Timer Progression

--- app/src/main/java/to426/com/peerpressure/RoundSplashActivity.java
+++ app/src/main/java/to426/com/peerpressure/RoundSplashActivity.java
@@ -1,11 +1,27 @@
 package to426.com.peerpressure;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.w3c.dom.Text;
 
 public class RoundSplashActivity extends AppCompatActivity {
+
+    public TextView roundTextView;
+    public TextView countdownTextView;
+
+    public String lobbyCode = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +31,58 @@ public class RoundSplashActivity extends AppCompatActivity {
         getSupportActionBar().setIcon(R.drawable.pplogo);
         getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#8b0000")));
 
+        roundTextView = (TextView) findViewById(R.id.roundTextView);
+
+        Intent retrieveCode = getIntent();
+        Bundle bundle = retrieveCode.getExtras();
+
+        if (bundle != null) {
+            lobbyCode = (String) bundle.get("lobbyCode");
+        }
+
+        countdownTextView = (TextView) findViewById(R.id.countdownTextView);
+
+        new CountDownTimer(6000, 1000) {
+            public void onFinish() {
+                Intent roundSplashToEnterDare = new Intent(RoundSplashActivity.this,REnterDareActivity.class);
+                roundSplashToEnterDare.putExtra("lobbyCode", lobbyCode);
+                startActivity(roundSplashToEnterDare);
+                
+                finish();
+            }
+
+            public void onTick(long millisUntilFinished) {
+                countdownTextView.setText("00:0" + millisUntilFinished / 1000);
+            }
+
+        }.start();
+
+        final DatabaseReference lobbyCheckRef = FirebaseDatabase.getInstance().getReference()
+                .child("Games").child(lobbyCode).child("Properties");
+
+        lobbyCheckRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                if (dataSnapshot.exists()) {
+
+                    GameProperties properties = dataSnapshot.getValue(GameProperties.class);
+
+                    if (properties.getGameProgression().equals("Round 1")) {
+
+                        roundTextView.setText("Round 1");
+                    }
+                }
+            }
+
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // ...
+            }
+        });
+
     }
+
+
 }
